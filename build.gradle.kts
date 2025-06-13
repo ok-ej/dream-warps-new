9da792a6c1f3575a6cb6706be443ff53ea80a7ec
ok_ej <98965280+ok-ej@users.noreply.github.com>
2025-06-13 22:14:42 +0200

    Add files via upload

---
+++ build.gradle.kts
@@ -0,0 +1,83 @@
+plugins {
+    `java-library`
+    id("idea")
+    id("com.gradleup.shadow") version "9.0.0-beta13"
+    id("io.papermc.paperweight.userdev") version "2.0.0-beta.16" apply false
+}
+
+idea {
+    project.jdkName = "23"
+}
+
+allprojects {
+    group = "cc.dreamcode.warp"
+    version = "1.0-InDEV"
+
+    apply(plugin = "java-library")
+    apply(plugin = "com.gradleup.shadow")
+
+    repositories {
+        mavenCentral()
+        maven("https://repo.dreamcode.cc/releases")
+        maven("https://storehouse.okaeri.eu/repository/maven-public")
+    }
+}
+
+subprojects {
+    java {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+
+        withSourcesJar()
+        withJavadocJar()
+    }
+
+    tasks.withType<JavaCompile> {
+        options.encoding = "UTF-8"
+        options.compilerArgs.add("-parameters")
+    }
+
+    dependencies {
+        /* General */
+        val lombok = "1.18.38"
+        compileOnly("org.projectlombok:lombok:$lombok")
+        annotationProcessor("org.projectlombok:lombok:$lombok")
+        testCompileOnly("org.projectlombok:lombok:$lombok")
+        testAnnotationProcessor("org.projectlombok:lombok:$lombok")
+    }
+}
+
+project(":plugin-core:nms").subprojects {
+
+    val minor = name.split("_").getOrNull(1)?.toInt() ?: 0
+    val patch = name.split("R").getOrNull(1)?.toInt() ?: 0
+
+    if (name == "api" || minor < 17) {
+        return@subprojects
+    }
+
+    apply(plugin = "io.papermc.paperweight.userdev")
+
+    if (minor >= 21 || minor == 20 && patch >= 4) {
+        java {
+            sourceCompatibility = JavaVersion.VERSION_21
+            targetCompatibility = JavaVersion.VERSION_21
+
+            withSourcesJar()
+            withJavadocJar()
+        }
+    }
+    else {
+        java {
+            sourceCompatibility = JavaVersion.VERSION_17
+            targetCompatibility = JavaVersion.VERSION_17
+
+            withSourcesJar()
+            withJavadocJar()
+        }
+    }
+}
+
+tasks.register("pluginVersion") {
+    println(project.version)
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "dream-warp"
+
+include(":plugin-core")
+
+include(":plugin-core:nms:api")
+include(":plugin-core:nms:v1_8_R3")
+include(":plugin-core:nms:v1_12_R1")
+include(":plugin-core:nms:v1_16_R3")
+include(":plugin-core:nms:v1_17_R1")
+include(":plugin-core:nms:v1_18_R2")
+include(":plugin-core:nms:v1_19_R2")
+include(":plugin-core:nms:v1_19_R3")
+include(":plugin-core:nms:v1_20_R1")
+include(":plugin-core:nms:v1_20_R2")
+include(":plugin-core:nms:v1_20_R3")
+include(":plugin-core:nms:v1_20_R5")
+include(":plugin-core:nms:v1_21_R1")
+include(":plugin-core:nms:v1_21_R2")
+include(":plugin-core:nms:v1_21_R3")
